89201c6f30f1021ebd73331a84c86d660f795860
Mayant Mukul <mayantmukul@outlook.in>
2021-02-01 17:07:47 +0530

    io: implement VGA support

--- kernel/main.rs
+++ kernel/main.rs
@@ -3,23 +3,16 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+    kprintln!("Hello kernel world!");
+    panic!("Testing a panic in main");
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    kprintln!("{}", info);
     loop {}
 }
+
+mod vga_buffer;
