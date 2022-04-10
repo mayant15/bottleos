e746577380a475812bb81b0a70316f299157f210
Mayant Mukul <mayantmukul@outlook.in>
2022-04-10 17:25:04 +0530

    feat(stivale): add wrapper to log with newline

--- src/stivale2.rs
+++ src/stivale2.rs
@@ -1,17 +1,19 @@
-extern {
+extern "C" {
     fn stivale2_init(ptr: u64) -> i8;
     fn stivale2_term_write(s: *const u8, len: usize);
 }
 
 pub fn init(ptr: u64) -> i8 {
-    unsafe {
-        stivale2_init(ptr)
-    }
+    unsafe { stivale2_init(ptr) }
 }
 
-pub fn write(s: &'static str) {
+pub fn write<'a>(s: &'a str) {
     unsafe {
         stivale2_term_write(s.as_ptr(), s.chars().count());
     }
 }
 
+pub fn writeln<'a>(s: &'a str) {
+    write(s);
+    write("\n");
+}
